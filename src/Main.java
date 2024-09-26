e4987248b234bb64f3ff89c6e3d74a02b8f163b3
mohamedAmineh <167142500+mohamedAmineh@users.noreply.github.com>
2024-09-26 21:35:13 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package projet_BDDA;
+
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
+
+
 public class Main {
-    public static void main(String[] args) {
-
-
-    }
+      public static void main(String [] args) {
+            
+            try {
+                     DBConfig config = DBConfig.loadDBConfig("../../infos.json");
+                     System.out.println(config.getDbpath());
+            
+        }catch(IOException e) {
+            System.out.println("erreur: " + e.getMessage());
+          }catch(ParseException e) {
+             System.out.println("erreur: " + e.getMessage());
+          }
+      }
+      
 }
