ba2c879a5e08401ea0e6886a6b98c3b148edc635
DorraChakour <136824295+DorraChakour@users.noreply.github.com>
2024-10-09 23:37:06 +0200

    Create BufferManager.java

---
+++ src/BufferManager.java
@@ -0,0 +1,24 @@
+public class BufferManager {
+    private DBConfig config;
+    private DiskManager diskManager;
+    private String currentPolicy;
+    private List<Buffer> buffers;
+
+    public BufferManager(DBConfig config, DiskManager diskManager) {
+        this.config = config;
+        this.diskManager = diskManager;
+        this.currentPolicy = config.getBmPolicy(); // Défini la politique initiale
+        this.buffers = new ArrayList<>(config.getBmBuffercount()); // Initialise les buffers selon la config
+    }
+
+    public void SetCurrentReplacementPolicy(String policy) {
+        if (policy.equals("LRU") || policy.equals("MRU")) {
+            currentPolicy = policy;
+            System.out.println("Politique de remplacement changée à : " + policy);
+        } else {
+            throw new IllegalArgumentException("Politique invalide : " + policy + ". Utilisez 'LRU' ou 'MRU'.");
+        }
+    }
+
+    // Autres méthodes ici (GetPage, FreePage, FlushBuffers, etc.)
+}
