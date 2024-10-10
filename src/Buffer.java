87ec2ad9f54d64915cd409756c021e7efe1542ce
sana <sana.benslama@yahoo.com>
2024-10-10 15:54:31 +0200

    classe Buffer

---
+++ src/Buffer.java
@@ -0,0 +1,45 @@
+import java.nio.ByteBuffer;
+
+public class Buffer {
+    private PageId pageId;
+    private ByteBuffer data;
+    private int pinCount;
+    private boolean dirty;
+
+    public Buffer(PageId pageId, ByteBuffer data) {
+        this.pageId = pageId;
+        this.data = data;
+        this.pinCount = 1;
+        this.dirty = false;
+    }
+    //getters
+    public PageId getPageId() {
+        return pageId;
+    }
+
+    public ByteBuffer getData() {
+        return data;
+    }
+
+    public int getPinCount() {
+        return pinCount;
+    }
+    public boolean isDirty(){
+        return dirty;
+    }
+
+    //setters
+    public void setData(ByteBuffer data) {
+        this.data = data;
+    }
+
+    public void setPinCount(int pinCount) {
+        this.pinCount = pinCount;
+    }
+
+    public void reset(){
+        pinCount=1;
+        dirty= false;
+    }
+}
+
